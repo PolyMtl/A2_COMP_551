646dbf04aa560057a8591a95e4d53911613e8032
Vivek Gidla <vivek.gidla@mail.mcgill.ca>
2018-02-05 20:01:29 -0500

    implemented opt_cov_matrix function -- need to test

--- Q2.py
+++ Q2.py
@@ -5,6 +5,21 @@
 import pandas as pd
 import numpy as np
 
+def get_class_samples(df):
+	samples_C0 = []
+	samples_C1 = []
+
+	for i,vector in df.iterrows():
+		vector = np.array(vector)
+		if(vector[20] == 1):
+			samples_C1.append(vector)
+		if(vector[20] == 0):
+			samples_C0.append(vector)
+
+	samples_C0 = np.array(samples_C0)
+	samples_C1 = np.array(samples_C1)
+	return samples_C0, samples_C1
+
 def calc_opt_pi(df):
 	N1 = 0
 	N2 = 0
@@ -16,20 +31,7 @@ def calc_opt_pi(df):
 
 	return float(N1)/(N1+N2)
 
-def calc_opt_mean_vectors(df):
-	samples_C0 = []
-	samples_C1 = []
-
-	for i,vector in training_set.iterrows():
-		vector = np.array(vector)
-		if(vector[20] == 1):
-			samples_C1.append(vector)
-		if(vector[20] == 0):
-			samples_C0.append(vector)
-
-	samples_C0 = np.array(samples_C0)
-	samples_C1 = np.array(samples_C1)
-
+def calc_opt_mean_vectors(samples_C0, samples_C1):
 	sum_C0 = np.zeros(20)
 	sum_C1 = np.zeros(20)
 	for j in samples_C0:
@@ -44,12 +46,39 @@ def calc_opt_mean_vectors(df):
 
 	return opt_mean_C0, opt_mean_C1
 
+def calc_opt_cov_matrix(samples_C0, samples_C1, opt_mean_C0, opt_mean_C1):
+	neg_mean_C0 = np.dot(-1,opt_mean_C0)
+	cov_C0 = np.zeros((20,20))
+	opt_cov = np.zeros((20,20))
+	for j in samples_C0:
+		j = j[:-1]
+		temp = np.add(j,neg_mean_C0)
+		cov_C0 = np.add(np.outer(temp,temp),cov_C0)
+	cov_C0 = np.dot((float(1)/1400),cov_C0)
+
+	neg_mean_C1 = np.dot(-1,opt_mean_C1)
+	cov_C1 = np.zeros((20,20))
+	for j in samples_C0:
+		j = j[:-1]
+		temp = np.add(j,neg_mean_C1)
+		cov_C0 = np.add(np.outer(temp,temp),cov_C1)
+	cov_C1 = np.dot((float(1)/1400),cov_C1)
+
+	opt_cov = np.add(cov_C0,cov_C1)
+	return opt_cov
+
 if __name__ == "__main__":
 	# get training set
 	training_set = pd.read_csv(r'/Users/vivek/git/A2_COMP_551/Datasets/DS1_train.csv', header = None).dropna(axis=1, how='any')
 	# need to calc optimal pi = N1/(N1+N2)
 	opt_pi = calc_opt_pi(training_set)
-	opt_mean_C0, opt_mean_C1 = calc_opt_mean_vectors(training_set)
+	samples_C0,samples_C1 = get_class_samples(training_set)
+	opt_mean_C0, opt_mean_C1 = calc_opt_mean_vectors(samples_C0,samples_C1)
+	opt_cov = calc_opt_cov_matrix(samples_C0,samples_C1,opt_mean_C0,opt_mean_C1)
+
 	
+	
+
+
 
 
