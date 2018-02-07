bc8fd0f4f231eacfeb94e2bcba07127206435dee
Vivek Gidla <vivek.gidla@mail.mcgill.ca>
2018-02-07 11:04:42 -0500

    fixed bug in cov matrix calculations, re ran Q1, therefore new (random) dataset

--- Q2.py
+++ Q2.py
@@ -1,9 +1,17 @@
 # Q2
+# need to model P(C_k | x)
+# model P(x|C_k) and P(C_k) and use Bayes law
+# in probabilistic LDA:
+# 	class conditional probabilities are guassian
+# 	all classes share same cov matrix
 # esitmate parameters of probabilistic LDA by maximizing likelihood function
 # parameters of likelihood function: DS1_m_0, DS1_m_1, DS1_Cov, P(C_0), P(C_1)
 # let P(C_0) = pi, P(C_1) = 1-pi 
+
+# C0 ---> label = 0, C1--->label =1
 import pandas as pd
 import numpy as np
+import math
 
 def get_class_samples(df):
 	samples_C0 = []
@@ -37,47 +45,45 @@ def calc_opt_mean_vectors(samples_C0, samples_C1):
 	for j in samples_C0:
 		j = j[:-1]
 		sum_C0 = np.array(np.add(sum_C0,j))
-	opt_mean_C0 = np.dot((float(1)/1400), sum_C0)
+	opt_mean_C0 = np.divide(sum_C0, len(samples_C0))
 
 	for k in samples_C1:
 		k = k[:-1]
 		sum_C1 = np.array(np.add(sum_C1,k))
-	opt_mean_C1 = np.dot((float(1)/1400), sum_C1)
+	opt_mean_C1 = np.divide(sum_C1, len(samples_C1))
 
 	return opt_mean_C0, opt_mean_C1
 
 def calc_opt_cov_matrix(samples_C0, samples_C1, opt_mean_C0, opt_mean_C1):
-	neg_mean_C0 = np.dot(-1,opt_mean_C0)
 	cov_C0 = np.zeros((20,20))
-	opt_cov = np.zeros((20,20))
 	for j in samples_C0:
 		j = j[:-1]
-		temp = np.add(j,neg_mean_C0)
-		cov_C0 = np.add(np.outer(temp,temp),cov_C0)
-	cov_C0 = np.dot((float(1)/1400),cov_C0)
+		tempj = np.zeros(20)
+		tempj = np.subtract(j,opt_mean_C0)
+		cov_C0 = cov_C0 + np.outer(tempj,tempj) 
 
-	neg_mean_C1 = np.dot(-1,opt_mean_C1)
 	cov_C1 = np.zeros((20,20))
-	for j in samples_C0:
-		j = j[:-1]
-		temp = np.add(j,neg_mean_C1)
-		cov_C0 = np.add(np.outer(temp,temp),cov_C1)
-	cov_C1 = np.dot((float(1)/1400),cov_C1)
-
-	opt_cov = np.add(cov_C0,cov_C1)
+	for k in samples_C1:
+		k = k[:-1]
+		tempk = np.zeros(20)
+		tempk = np.subtract(k,opt_mean_C1)
+		cov_C1 = cov_C1 + np.outer(tempk,tempk) 
+	
+	opt_cov = np.zeros((20,20))
+	opt_cov = np.divide(np.add(cov_C0,cov_C1),2800)
 	return opt_cov
 
 if __name__ == "__main__":
 	# get training set
 	training_set = pd.read_csv(r'/Users/vivek/git/A2_COMP_551/Datasets/DS1_train.csv', header = None).dropna(axis=1, how='any')
-	# need to calc optimal pi = N1/(N1+N2)
+
 	opt_pi = calc_opt_pi(training_set)
 	samples_C0,samples_C1 = get_class_samples(training_set)
 	opt_mean_C0, opt_mean_C1 = calc_opt_mean_vectors(samples_C0,samples_C1)
 	opt_cov = calc_opt_cov_matrix(samples_C0,samples_C1,opt_mean_C0,opt_mean_C1)
-
-	
-	
+	# P(C_k | x)
+	w = np.dot(np.linalg.inv(opt_cov),np.subtract(opt_mean_C1,opt_mean_C0))
+	w_0 = np.divide((np.dot(np.dot(opt_mean_C1.T,np.linalg.inv(opt_cov)),opt_mean_C1)),-2) + np.divide((np.dot(np.dot(opt_mean_C0.T,np.linalg.inv(opt_cov)),opt_mean_C0)),2) + np.log(opt_pi/(1-opt_pi))
 
 
 
